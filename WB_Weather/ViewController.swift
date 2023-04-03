2dd814c194c59f73b21ec8b493f418ee3a678258
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-04-03 09:44:47 +0300

    Initial Commit

---
+++ WB_Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WB_Weather
+//
+//  Created by Лёха Небесный on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
